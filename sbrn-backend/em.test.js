f9aa0df406005dd37901662016c81e4386af3b97
mustafa20170986 <jster0986@gmail.com>
2026-01-10 22:03:54 -0800

    ci pipeline first

---
+++ sbrn-backend/em.test.js
@@ -0,0 +1,14 @@
+import request from 'supertest'
+import {jest} from '@jest/globals'
+import{sbrn} from './node.js'
+
+describe("testing mini",()=>{
+    test('testing normal ci',async()=>{
+        const req=await request(sbrn)
+        .get("/sbrn-get")
+
+        expect(req.status).toBe(200)
+        expect(req.body.message).toBe("message from backend")
+    })
+})
+
---
+++ sbrn-backend/node.js
@@ -0,0 +1,14 @@
+import express from "express" 
+import cors from "cors"
+
+export const sbrn=express()
+sbrn.use(cors())
+sbrn.use(express.json())
+
+sbrn.get("/sbrn-get",async(req,res)=>{
+    res.json({
+        message:"message from backend"
+    })
+})
+
+
